0dada8f2f54a58a1887ac53e3ad3f0db1f42a240
khabert <khabertzion11@gmail.com>
2022-12-10 07:34:52 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
-  * main - Entry Point
-  *
-  * Return: Always 0 (Success)
-  */
+*main - Entry Point
+* Return: Always 0 (Success)
+*/
 
 int main(void)
 {
@@ -14,6 +13,4 @@ int main(void)
 	printf(*Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf(*Size of a foat: %lu byte(s)\n", sizeof(float));
 	return (0);
-
-
 }
