1c7ffbcabc7581a3688e42420fcbd92285fcc8ec
khabert <khabertzion11@gmail.com>
2023-01-04 07:03:50 -0800

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
   * _puts_recursion - function like puts();
   *@s: input
@@ -8,9 +10,9 @@ void _puts_recursion(char *s)
 {
 	if (*s)
 	{
-		_putchar(*s);
+		putchar(*s);
 		_puts_recursion(s + 1);
 	}
 	else
-		_putchar('\n');
+		putchar('\n');
 }
