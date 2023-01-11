39e3433ea025d3d7792ff4dd24aea57b0c6851a3
khabert <khabertzion11@gmail.com>
2023-01-11 07:11:22 -0800

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,8 +11,9 @@
 char *str_concat(char *s1, char *s2)
 
 {
-	char *concat;
-i	int i, ci++;
+	char *conct;
+
+	int i, ci;
 
 	if (s1 == NULL)
 
@@ -32,24 +33,24 @@ i	int i, ci++;
 
 		ci++;
 
-	concat = malloc(sizeof(char) * (i +ci + 1));
+	conct = malloc(sizeof(char) * (i + ci + 1));
 
-	if (concat == NULL)
+	if (conct == NULL)
 
 		return (NULL);
 	i = ci = 0;
 	while (s1[i] != '\0')
 	{
-		concat[i] = s1[i];
+		conct[i] = s1[i];
 
 		i++;
 	}
 	while (s2[ci] != '\0')
 	{
-		concat[i] = s2[ci];
+		conct[i] = s2[ci];
 		ci++;
 	}
-	concat[i] = '\0';
+	conct[i] = '\0';
 
-	return (concat);
+	return (conct);
 }
