c8f11ccc47f9cd8fb2b3999f8fb836d3c11f4fdd
khabert <khabertzion11@gmail.com>
2023-01-18 07:28:46 -0800

    0-print_name.c

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,10 @@
+#ifndef
+#define
+
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+./main number_of_bytes
+calc num1 operator num2
+
+#endif 
